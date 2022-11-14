cb68d8a3f8fb927042c91105b19a30dc632d7a07
minh <hadtnt71@gmail.com>
2022-11-15 01:38:50 +0700

    sign in

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,12 +1,127 @@
-import React from "react";
+import React, { useState } from "react";
 import Styled from "./style";
+import { useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
+import VisibilityOffOutlinedIcon from "@mui/icons-material/VisibilityOffOutlined";
 const SignIn = () => {
+  const [showPwd, setShowPwd] = useState(false);
+  const navigate = useNavigate();
+  const signInSchema = Yup.object({
+    username: Yup.string()
+      .email("Not a proper email")
+      .min(10, "Minimum 10 characters")
+      .required("Username required"),
+    password: Yup.string()
+      .required("No password provided.")
+      .min(8, "Password is too short - should be at least 8 characters")
+      .matches(/^(?=.*[A-Z])/, "Must contain at least one uppercase character")
+      .matches(/^(?=.*[0-9])/, "Must contain at least one number")
+      .matches(/^(?=.*[!@#%&])/, "Must contain at least one special character")
+  });
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: ""
+    },
+    validationSchema: signInSchema,
+    onSubmit: async (value) => {
+      console.log("value submit ", value);
+    }
+  });
   return (
     <Styled>
       <div className="signin-container">
         <div className="header">
           <img src="./kahoot.png" className="header-img" alt="kahoot" />
         </div>
+        <main class="signin-main">
+          <div className="main-container">
+            <div className="auth-form">
+              <div className="card-container">
+                <h2>Log in</h2>
+                <form className="form-login" method="post" onSubmit={formik.handleSubmit}>
+                  <div className="input-box">
+                    <label htmlFor="username" className="input-label">
+                      Email
+                    </label>
+                    <input
+                      id="username"
+                      name="username"
+                      value={formik.values.username}
+                      onChange={formik.handleChange}
+                      type="text"
+                      placeholder="Input username"
+                      className="input-text"
+                    />
+                    {formik.errors.username && formik.touched.username && (
+                      <p className="error-message">{formik.errors.username}</p>
+                    )}
+                  </div>
+                  <div className="input-box">
+                    <label htmlFor="password" className="input-label">
+                      Password
+                    </label>
+                    <div className="pwd-container">
+                      <input
+                        id="password"
+                        name="password"
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                        type={showPwd ? "text" : "password"}
+                        placeholder="Input password"
+                        className="input-text"
+                      />
+                      <div className="pwd-action" onClick={() => setShowPwd(!showPwd)}>
+                        <div className="pwd-img">
+                          {showPwd == false ? (
+                            <VisibilityOutlinedIcon />
+                          ) : (
+                            <VisibilityOffOutlinedIcon />
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                    {formik.errors.password && formik.touched.password && (
+                      <p className="error-message">{formik.errors.password}</p>
+                    )}
+                  </div>
+                  <button type="submit" className="login-btn">
+                    Log in
+                  </button>
+                </form>
+                <div className="auth-split">
+                  <hr className="card-line" />
+                  <span className="card-text">or</span>
+                </div>
+                <div className="single-sign-on">
+                  <button className="google-sign">
+                    <img
+                      className="google-sign-img"
+                      src="https://assets-cdn.kahoot.it/auth/assets/google.004af66e.svg"
+                      alt="google"
+                    />
+                    <div className="google-sign-label">Continue with google </div>
+                  </button>
+                </div>
+                <p className="redirect-signup">
+                  Don't have an account?
+                  <a href="/signup">Sign up</a>
+                </p>
+              </div>
+            </div>
+            <p className="text-disclaimer">
+              By signing up, you accept our Terms and Conditions. Please read our Privacy Policy and
+              Children’s Privacy Policy.
+            </p>
+            <p className="text-disclaimer">
+              I understand that I can withdraw my consent at any time and the withdrawal will not
+              affect the lawfulness of the consent before its withdrawal, as described in the
+              Kahoot! Privacy Policy.
+            </p>
+          </div>
+        </main>
       </div>
     </Styled>
   );
--- src/pages/SignIn/style.jsx
+++ src/pages/SignIn/style.jsx
@@ -1,12 +1,192 @@
 import styled from "styled-components";
 const Styled = styled.div`
   .signin-container {
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
   }
   .header {
+    width: 100%;
     position: fixed;
     left: 0px;
     top: 0px;
     z-index: 2;
+    height: 3.5rem;
+    box-shadow: rgb(0 0 0 / 10%) 0px 2px 4px 0px;
+    background: #fff;
+    padding: 10px 20px;
+  }
+  .header-img {
+    width: 5rem;
+  }
+  .signin-main {
+    padding-top: 3.5rem;
+    width: 100%;
+    height: 100%;
+    background-color: transparent;
+  }
+  .main-container {
+    display: flex;
+    flex-direction: column;
+    max-width: 400px;
+    margin: 0 auto;
+    width: fit-content;
+  }
+  .card-container {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: rgb(255, 255, 255);
+    border-radius: 10px;
+    box-shadow: rgb(0 0 0 / 15%) 0px 2px 4px 0px;
+    padding: 10px 40px;
+  }
+  .card-container h2 {
+    font-weight: bold;
+    line-height: 140%;
+    margin-top: 30px;
+  }
+  .input-box {
+    align-self: flex-start;
+    width: 100%;
+  }
+  .input-label {
+    font-weight: bold;
+    display: block;
+    padding: 5px 0px;
+  }
+  .input-box .input-text[type="text"],
+  .input-box .input-text[type="password"] {
+    width: 100%;
+    min-height: 2.75rem;
+    font-family: Montserrat, "Noto Sans Arabic", "Helvetica Neue", Helvetica, Arial, sans-serif;
+    border: 1px solid rgb(178, 178, 178);
+    border-radius: 5px;
+    background-color: #fff !important;
+    color: rgb(51, 51, 51) !important;
+    font-size: 1rem;
+    line-height: 1.25rem;
+    letter-spacing: 0.2px;
+    outline: none;
+    border: none;
+    transition: all 1s ease-in;
+    padding-left: 20px;
+    text-overflow: ellipsis;
+  }
+  .input-text[type="password"] {
+    padding-right: 35px;
+    overflow: hidden;
+    white-space: nowrap;
+  }
+  .input-text:focus {
+    border: 1px solid rgb(19, 104, 206) !important;
+  }
+  .login-btn {
+    margin: 30px 0px;
+    border: none;
+    width: 100%;
+    cursor: pointer;
+    box-shadow: rgb(0 0 0 / 25%) 0px -4px inset;
+    background: rgb(38, 137, 12);
+    color: rgb(255, 255, 255);
+    border-radius: 4px;
+    font-size: 0.875rem;
+    font-weight: bold;
+    text-align: center;
+    text-decoration: none;
+    min-width: 42px;
+    min-height: 42px;
+    padding: 0px 16px 4px;
+  }
+  .card-line {
+    margin: 0px;
+    width: 100%;
+    border-top: 1px solid rgb(204, 204, 204);
+  }
+  .auth-split {
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    position: relative;
+    width: 100%;
+    flex-direction: row;
+  }
+  .card-text {
+    position: absolute;
+    padding: 0px 0.5rem;
+    background: rgb(255, 255, 255);
+    font-weight: bold;
+    margin-top: -0.6rem;
+    left: 50%;
+    transform: translateX(-50%);
+    z-index: 1;
+  }
+  .single-sign-on {
+    width: 100%;
+    padding: 1.5rem 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+  .google-sign {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    margin: 0.25rem 0px;
+    border-radius: 4px;
+    color: rgb(0, 0, 0);
+    border: 1px solid rgb(0, 0, 0);
+    background-color: rgb(255, 255, 255);
+    box-shadow: none;
+    padding: 0px;
+    line-height: 1rem;
+    height: auto;
+    font-weight: 500;
+  }
+  .google-sign-img {
+    min-height: 1.75rem;
+    margin: 0px 0.5rem;
+  }
+  .form-login {
+    width: 100%;
+  }
+  .text-disclaimer {
+    margin-top: 1rem;
+    max-width: 26rem;
+    text-align: center;
+    font-size: 0.8rem;
+    color: rgb(110, 110, 110);
+  }
+  .error-message {
+    color: red;
+    font-size: 14px;
+    line-height: 20px;
+    margin: 20px 0px;
+  }
+  .redirect-signup {
+    text-align: center;
+    letter-spacing: 1px;
+  }
+  .redirect-signup a {
+    color: rgb(19, 104, 206);
+    margin-left: 5px;
+  }
+  .pwd-container {
+    position: relative;
+  }
+  .pwd-action {
+    position: absolute;
+    right: 0.5rem;
+    top: 50%;
+    transform: translateY(-50%);
+    height: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
   }
 `;
 
