0318fb8dfa0d8f4f16b7afac0e10b665a5b28ac7
SuperDMWebDev <hadtnt71@gmail.com>
2022-11-14 21:12:49 +0700

    create login

---
+++ src/components/login/style.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+const Styled = styled.div`
+  .signin-container {
+    display: flex;
+    flex-direction: column;
+    max-width: 400px;
+    width: 400px;
+  }
+  .signin-title {
+    font-size: 60px;
+    color: white;
+    margin-bottom: 40px;
+    text-align: center;
+  }
+`;
+
+export default Styled;
