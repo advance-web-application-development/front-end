c4c7ae7137fd2e718198616d41de7354fe4a6489
Minh Duy Pham <hadtnt71@gmail.com>
2022-11-28 23:14:56 +0700

    update header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import Container from "react-bootstrap/Container";
 import Avatar from "@mui/material/Avatar";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
@@ -14,16 +14,19 @@ import ListItemText from "@mui/material/ListItemText";
 import PersonIcon from "@mui/icons-material/Person";
 import { Link } from "react-router-dom";
 import { onLogout } from "../../utils/method.jsx";
+import UserContext from "../../utils/UserContext.jsx";
 
 // Lấy 2 chữ cái đầu của tên cho Avatar.
 function stringAvatar(name) {
   return {
-    children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`
+    children: `${name.split(" ")[0][0]}`
   };
 }
 
 export const Header = function (props) {
   const [hasSignedIn, setHasSignedIn] = useState(true);
+  const [currentUser, setCurrentUser] = useContext(UserContext);
+  console.log("header currentUser ", currentUser);
   if (hasSignedIn) {
     return (
       <>
@@ -32,7 +35,6 @@ export const Header = function (props) {
             <MenuBar.Brand href="/">
               <img id="logo" style={{ width: "4rem" }} src="/assets/images/google-classroom.png" />
             </MenuBar.Brand>
-            {/* <MenuBar.Toggle aria-controls="navbar-nav" /> */}
             <MenuList className="me-auto">
               <MenuBarItem to="/home">Home</MenuBarItem>
               <MenuBarItem to="/groups">Groups</MenuBarItem>
@@ -44,7 +46,7 @@ export const Header = function (props) {
               </CreatingButton>
               <AvatarButton>
                 <Avatar
-                  {...stringAvatar("Kent Dodds")}
+                  {...stringAvatar(currentUser?.username || "Incognito")}
                   className="bg-success"
                   role="button"
                   sx={{ fontSize: "1.6rem" }}
@@ -240,4 +242,3 @@ function AvatarButton(props) {
     </>
   );
 }
-//#endregion
